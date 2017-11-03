6d3dd674862d4fdf2e100a0e16957eca8a69242c
giansalex <giansalex@gmail.com>
2017-11-03 17:21:34 -0500

    add sample data + endpoint xml to json

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,6 +1,11 @@
 <?php
 // DIC configuration
 
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 $container = $app->getContainer();
 
 // monolog
@@ -10,4 +15,19 @@ $container['logger'] = function ($c) {
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
+};
+
+$container['serializer'] = function ($c) {
+    $encoders = array(new JsonEncoder());
+    $normalizers = array(new DateTimeNormalizer(), new GetSetMethodNormalizer());
+
+    return new Serializer($normalizers, $encoders);
+};
+
+$container['parser.invoice'] = function ($c) {
+    return new \Greenter\Xml\Parser\InvoiceParser();
+};
+
+$container['dir_xml'] = function ($c) {
+    return __DIR__.'/data/';
 };
--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,24 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/{ruc}', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $ruc = $args['ruc'];
+$app->get('/[{ruc}]', function (Request $request, Response $response, array $args) {
+    //$ruc = $args['ruc'];
 
-    // Render index view
-    return $response->withJson($ruc);
+    /**@var $parser \Greenter\Parser\DocumentParserInterface */
+    $parser = $this->get('parser.invoice');
+    /**@var $serializer \Symfony\Component\Serializer\Serializer */
+    $serializer = $this->get('serializer');
+    $files = glob($this->get('dir_xml') . '*.xml');
+    $documents = [];
+    foreach ($files as $file) {
+        if($file == '.' || $file == '..') continue;
+
+        $document = $parser->parse(file_get_contents($file));
+        $documents[] = $document;
+    }
+
+    $json = $serializer->serialize($documents, 'json');
+    $response->getBody()->write($json);
+
+    return $response->withHeader('Content-Type', 'application/json; charset=utf8');
 });

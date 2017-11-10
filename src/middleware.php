681a06443336ce827247b4799fbc48a1ffa2f57a
Giancarlos Salas <giansalex@gmail.com>
2017-11-09 23:25:11 -0500

    enable secure routes using JWT

--- src/middleware.php
+++ src/middleware.php
@@ -4,14 +4,14 @@
 $container = $app->getContainer();
 
 // JWT
-//$app->add(new \Slim\Middleware\JwtAuthentication([
-//    "secure" => false,
-//    "attribute" => "jwt",
-//    "path" => '/api',
-//    "passthrough" => ["/api/v1/login", "/api/v1/register"],
-//    "secret" => $container['settings']['jwt']['secret'],
-//    "algorithm" => ["HS256"],
-//]));
+$app->add(new \Slim\Middleware\JwtAuthentication([
+    "secure" => false,
+    "attribute" => "jwt",
+    "path" => '/api',
+    "passthrough" => ["/api/v1/login", "/api/v1/register"],
+    "secret" => $container['settings']['jwt']['secret'],
+    "algorithm" => ["HS256"],
+]));
 
 $app->add(new \Tuupola\Middleware\Cors([
     "origin" => ["*"],
